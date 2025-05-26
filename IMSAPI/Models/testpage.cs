be0bee9c340a1a684e3719c66457af0522c764c8
AbhishekPanuganti <49441680+AbhishekPanuganti@users.noreply.github.com>
2025-05-26 11:18:06 +0530

    Add files via upload

---
+++ IMSAPI/Models/testpage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMSAPI.Models
+{
+    public class testpage
+    {
+        public int id { get; set; }
+        public string globalid { get; set; }
+        public string empname { get; set; }
+        public string action { get; set; }
+
+    }
+}
