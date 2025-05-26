01099dc54dbda85b8941c435b9537cf34b370ce6
AbhishekPanuganti <49441680+AbhishekPanuganti@users.noreply.github.com>
2025-05-26 11:11:00 +0530

    Add files via upload

---
+++ IMSAPI/AntiXssMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMSAPI
+{
+    public class AntiXssMiddleware
+    {
+    }
+
+    public class ErrorResponse
+    {
+        public int ErrorCode { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ IMSAPI/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Configuration;
+
+[assembly: OwinStartup(typeof(IMSAPI.Startup))]
+
+namespace IMSAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string key = "IMSAPPLICATIONNLLRQSML4756789SECRETKEYaqfgrysdjsfihwiuhiu"; //Secret key which will be used later during validation  
+            string iss_usr = ConfigurationManager.AppSettings["ISSUSER"].ToString();
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = iss_usr, //some string, normally web url,  
+                        ValidAudience = iss_usr,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                    }
+                });
+        }
+    }
+}
